8fb7104e2612e8d46d2c1449298443b51a3eb047
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-26 10:53:10 +0900

    Try to divide css file and .svelte file.

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,6 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<link rel="stylesheet" tpye="text/css" href="">
-<!-- <style >
+<style>
 	
-</style> -->
+</style>
