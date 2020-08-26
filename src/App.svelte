8081c42b0b527ac0292dab420d75fe0874496728
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-26 11:04:37 +0900

    Renamed let-variable 'name'.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,9 @@
 <script>
-	export let name;
+	let name = "Vincent";
 </script>
 
 <main>
-	<h1>Hello World!!</h1>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
