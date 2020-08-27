f93bf533050ba7615d884299b9a6b27219d3dab8
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-27 21:49:25 +0900

    Applying a unique key to each element inside our array, and pass that unique key-property to the loop.

--- src/App.svelte
+++ src/App.svelte
@@ -44,7 +44,7 @@
 
 	let people = [
 		{name: 'Vincent', beltColour: 'black', age: 19, id: 1},
-		{name: 'Stewart', beltColour: 'orange', age: 16, id: 2},
+		{name: 'Stuart', beltColour: 'orange', age: 16, id: 2},
 		{name: 'Ralph', beltColour: 'brown', age: 18, id: 3},
 	];
 </script>
@@ -68,8 +68,24 @@
 	<!-- svelte-ignore a11y-missing-attribute -->
 	<img src={imgSrc}>
 
-	 <!-- Almost like a for-each Loop. -->
-	{#each people as person}
-	
+	<!-- 
+		1. Almost like a for-each Loop. 
+		2. Applying a unique key to each element inside our array
+		and pass that unique key-property to the loop.
+		3. (person.id) of '()' is a parentheses.
+	-->
+	{#each people as person (person.id)}
+	  <div>
+		  <h4>{person.name}</h4>
+		  <p>{person.age} years old, {person.beltColour} belt.</p>
+	  </div>
+	<!-- 
+	    1. Adding inside each-block another keyword and
+		it's going to be colon else inside here.
+		2. Comment out all of list-contents in variable 'people' and
+		save it, then you can see, 'There are no people to show.'.
+	-->
+	{:else}
+	  <p>There are no people to show...</p>
 	{/each}
 </main>
