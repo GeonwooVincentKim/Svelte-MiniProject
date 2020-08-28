ffb35426e043d8c0ecf5705104d08caee9ed6572
kdsnop@gmail.com <kdsnop@gmail.com>
2020-08-28 20:31:41 +0900

    Move previous contents of svelte file to 'App.svelte' again.

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,47 @@
 <script>
+	let firstName = "Vincent";
+	let lastName = "Kim";
+	
+	/*
+		To output the result of beltColour right here,
+		Delete "color: {beltColor}".
+	*/
+	let beltColour = "black";
+
+	/* 
+	    The Reactive-Value.
+	    - 1. Use a template string to concatenate firstName and lastName
+		regularly.
+		
+		- 2. Reactive-Statement is watching whatever values we use
+		inside it. In this case, 'beltColour'.
+	*/
+	$: fullName = `${firstName} ${lastName}`;
+
+	// Run this code-blocks statement everytime.
+	$: {
+		console.log(beltColour);
+		console.log(fullName);
+	}
+
+	/*
+	  1. Make sure you have to convert file-form
+		 as 'png' not 'jpg'.
+	  
+	  2. You should save your file into public file-directory,
+	     not to create a new one in file-directory named 'src'.
+	*/
+	let imgSrc = "images.png";
+
+	const handleClick_colour = () => {
+		beltColour = 'orange';
+	};
+	
+	const handleInput = (e) => {
+		// Update on Screen in Real-Time.
+		beltColour = e.target.value;
+	};
+	
 	let people = [
 		{name: 'Vincent', beltColour: 'black', age: 19, id: 1},
 		{name: 'Stuart', beltColour: 'orange', age: 16, id: 2},
@@ -15,6 +58,24 @@
 </script>
 
 <main>
+	<!-- Output full-name here. -->
+	<p style="color: {beltColour}">{fullName} - has {beltColour} belt</p>
+	<button on:click={handleClick}>Update belt colour</button>
+
+	<!-- Two way bounding, 'handleInput' and 'beltColour'. -->
+	<!-- <input type="text" on:input={handleInput} value={beltColour}> -->
+	
+	<input type="text" bind:value={firstName}>
+	<input type="text" bind:value={lastName}>
+	<!-- 
+		One way bounding, but it controlls all of things 
+		by using 'beltColour'. 
+	-->
+	<input type="text" bind:value={beltColour}>
+
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img src={imgSrc}>
+	
 	<!-- 
 		1. Almost like a for-each Loop. 
 		2. Applying a unique key to each element inside our array
